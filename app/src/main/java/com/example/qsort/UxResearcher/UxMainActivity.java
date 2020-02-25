b1e3fa8ec83cde56466e7927a2215b2c9d7665f2
Hongyu Liu <hongyu.liu@mail.utoronto.ca>
2020-02-24 22:21:41 -0500

    project created

--- app/src/main/java/com/example/qsort/UxResearcher/UxMainActivity.java
+++ app/src/main/java/com/example/qsort/UxResearcher/UxMainActivity.java
@@ -2,15 +2,105 @@ package com.example.qsort.UxResearcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.qsort.R;
 
-public class UxMainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class UxMainActivity<map> extends AppCompatActivity {
+
+    static int FILE_REQUEST_CODE = 1;
+    Uri filePath;
+    TextView path;
+    Map<String,List<String>> projectMap =new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ux_main);
+
+        path=findViewById(R.id.pathTextView);
     }
+
+    public void addProject(View view){
+        Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        fileIntent.setType("text/csv");
+        startActivityForResult(fileIntent,FILE_REQUEST_CODE);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_REQUEST_CODE
+                && resultCode == RESULT_OK
+                && data != null) {
+            filePath = data.getData();
+            try {
+                readTextFromUri(filePath);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+    private void readTextFromUri(Uri uri) throws IOException {
+        StringBuilder categories = new StringBuilder();
+        StringBuilder labels = new StringBuilder();
+
+        try (InputStream inputStream =
+                     getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] element = line.split(",");
+                if(!element[0].equals("")){
+                    categories.append(element[0]+"\n");
+                }
+                if(!element[1].equals("")){
+                    labels.append(element[1]+"\n");
+                }
+            }
+
+            Intent intent = new Intent(getApplicationContext(),UxProjectSettingsActivity.class);
+
+            intent.putExtra("Categories",categories.toString());
+            intent.putExtra("Labels",labels.toString());
+            // start the activity
+            startActivity(intent);
+//
+//            System.out.println(categories);
+//            System.out.println(labels);
+
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/qsort/UxResearcher/UxProjectSettingsActivity.java
+++ app/src/main/java/com/example/qsort/UxResearcher/UxProjectSettingsActivity.java
@@ -1,16 +1,102 @@
 package com.example.qsort.UxResearcher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.qsort.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UxProjectSettingsActivity extends AppCompatActivity {
+
+    String categories, labels;
+    EditText categoriesTextView,labelsTextView,projectTitleTextView;
+
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ux_project_settings);
+
+        categoriesTextView = findViewById(R.id.categoryTextView);
+        labelsTextView = findViewById(R.id.labelTextView);
+        projectTitleTextView = findViewById(R.id.projectTitleTextView);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        Intent intent = getIntent();
+
+        categories = intent.getExtras().getString("Categories");
+        labels = intent.getExtras().getString("Labels");
+
+        categoriesTextView.setText(categories);
+        labelsTextView.setText(labels);
     }
+
+    public void submitProject(View view){
+
+        String[] categoriesArray = categoriesTextView.getText().toString().split("\n");
+        String[] labelsArray = labelsTextView.getText().toString().split("\n");
+        String projectTitle = projectTitleTextView.getText().toString();
+        String timestamp = String.valueOf(Timestamp.now().getSeconds());
+
+        for (int i=0; i<labelsArray.length;i++){
+            Map<String, Integer> label = new HashMap<>();
+
+            for (int j=0; j<categoriesArray.length;j++){
+                label.put(categoriesArray[j],0);
+            }
+            firebaseFirestore.collection(timestamp).document(labelsArray[i])
+                    .set(label)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            showMessage("Project successfully created!");
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("Error writing document", e);
+                        }
+                    });
+        }
+
+        Map<String, String> project = new HashMap<>();
+        project.put("Project Name",projectTitle);
+        project.put("Project ID",timestamp);
+
+        firebaseFirestore.collection("projects").document(timestamp).set(project)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startActivity(new Intent(getApplicationContext(),UxMainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+
+    }
+
 }
