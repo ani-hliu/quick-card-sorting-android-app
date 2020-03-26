1c601d16a9d2b5697341b32ec92cd096cbcc0335
Hongyu Liu <hongyu.liu@mail.utoronto.ca>
2020-03-26 16:43:13 -0400

    review for researcher

--- app/src/main/java/com/example/qsort/UxResearcher/ObjectFragment.java
+++ app/src/main/java/com/example/qsort/UxResearcher/ObjectFragment.java
@@ -1,26 +1,20 @@
-package com.example.qsort.Participants;
+package com.example.qsort.UxResearcher;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.example.qsort.R;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import q.rorbin.verticaltablayout.VerticalTabLayout;
 
 
 public class ObjectFragment extends Fragment {
@@ -33,36 +27,37 @@ public class ObjectFragment extends Fragment {
     Context context;
     CharSequence title;
     List<String> labelInCateList;
-
+    int index;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.fragment_card, container, false);
+                R.layout.ux_fragment_card, container, false);
         context = container.getContext();
 
         return rootView;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         Bundle args = getArguments();
         result = args.getString(RESULT_KEY);
+//        System.out.println(result);
         labels = args.getString(LABEL_KEY);
         title = args.getString("title");
         resultArray = result.split(",");
         labelsArray = labels.split("\n");
         labelInCateList = new ArrayList<>();
-
-        for(int i=0; i <resultArray.length;i++){
-            if(resultArray[i].equals(title.toString())){
-                labelInCateList.add(labelsArray[i]);
+        for(int i=0; i<labelsArray.length; i++){
+            if(title.toString().equals(labelsArray[i])){
+                index =i;
             }
         }
 
-        TextView labelResult = (TextView) view.findViewById(R.id.labelResult);
-        labelResult.setText(transferToString(labelInCateList));
+        TextView labelResult = (TextView) view.findViewById(R.id.UXlabelResult);
+        labelResult.setText(resultArray[index]);
 
     }
 
